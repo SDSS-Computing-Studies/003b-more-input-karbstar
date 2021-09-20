5e63efbcaa79cd5c0d5a574411a859c26e9c94d5
karbstar <90715867+karbstar@users.noreply.github.com>
2021-09-20 14:20:06 -0700

    df

--- task4.py
+++ task4.py
@@ -12,16 +12,16 @@ Today we picked apple from _PERSON_'s Orchard. I had no idea there were so many 
 from typing import DefaultDict
 
 
-PERSON =(input("enter a name")).strip
-ADJECTIVE=(input("enter a adjective")).strip
-FOOD=(input("enter a food")).strip
-ADJECTIVE2=(input("enter a adjective")).strip
-NOUN=(input("enter a noun")).strip
-PLACE=(input("enter a place")).strip
-VERB=(input("enter a verb")).strip
-ADVERB=(input("enter a adverb")).strip
-FOOD2=(input("enter a food")).strip
-THINGS=(input("enter a thing")).strip
+PERSON =(input("enter a name")).strip()
+ADJECTIVE=(input("enter a adjective")).strip()
+FOOD=(input("enter a food")).strip()
+ADJECTIVE2=(input("enter a adjective")).strip()
+NOUN=(input("enter a noun")).strip()
+PLACE=(input("enter a place")).strip()
+VERB=(input("enter a verb")).strip()
+ADVERB=(input("enter a adverb")).strip()
+FOOD2=(input("enter a food")).strip()
+THINGS=(input("enter a thing")).strip()
 
 
 print(f"Today we picked apple from {PERSON}'s Orchard. I had no idea there were so many different varieties of apples. I ate {ADJECTIVE} apples straight off the tree that tasted like {FOOD}. Then there was a {ADJECTIVE2} apple that looked like a {NOUN}.  When our bag was full, we went on a free hay ride to {PLACE} and back. It ended at a hay pile where we got to {VERB} {ADVERB}. I can hardly wait to get home and cook with the apples. We are going to make appple {FOOD2} and {THINGS} pies!")
